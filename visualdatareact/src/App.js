4181cb4f77b7bfa5cf34a1a24d29aca43d486b00
LeonXZhou <leon.zhou@queensu.ca>
2022-02-22 17:27:14 -0800

    refactored x and y data code to more easily send to backend

--- visualdatareact/src/App.js
+++ visualdatareact/src/App.js
@@ -1,33 +1,29 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useEffect } from "react";
 
+//styles
 import './App.scss';
+
+//components
 import NavBar from "./components/navigation/NavBarTop"
 import Home from "./components/home/Home";
 import BasicVisualization from "./components/basicVisualization/BasicVisualization";
 
-
-import axios from 'axios';
-import Cookies from 'js-cookie'
+//helpers
+import { getCsrfCookie, calcLinearFit } from "./helpers/apiHelpers";
 
 export default function App() {
+
+  useEffect(() => { getCsrfCookie() }, [])
+
   return (
     <BrowserRouter>
       <NavBar></NavBar>
       <div className={'contentBody'}>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/" element={<BasicVisualization />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/data" element={<BasicVisualization />} />
         </Routes>
-        <button onClick={() => {
-          axios.get('/api/')
-            .then((res) => {
-              console.log(res)
-              console.log('cookie', Cookies.get('csrftoken'))
-              axios.post('/api/', { asdf: 'oops' },
-                { headers: { 'X-CSRFToken': Cookies.get('csrftoken') } })
-                .then((res) => { console.log(res) })
-            })
-        }}>what</button>
       </div>
     </BrowserRouter>
 
