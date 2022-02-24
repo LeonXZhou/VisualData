1d01f0f39c3268a06a1aed2115908b193f83ab92
LeonXZhou <leon.zhou@queensu.ca>
2022-02-23 18:45:40 -0800

    implemented linspace for xvals and also calulated yvals based on regression

--- visualdatadjango/api/linear_fit_views.py
+++ visualdatadjango/api/linear_fit_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from scipy.stats import linregress
+import numpy as np
 # Create your views here.
 
 
@@ -21,8 +22,13 @@ def test(request):
 
 def linear(request):
     request_data = json.loads(request.body)
+
+    if len(request_data['xVals']) == 0:
+        return JsonResponse({'data':[]}) 
+        
     xdata = []
     ydata = []
+
     for xVal in request_data['xVals']:
         xdata.append(float(xVal))
     for yVal in request_data['yVals']:
@@ -30,4 +36,16 @@ def linear(request):
 
     slope, intercept, r, p, se = linregress(xdata, ydata)
 
-    return JsonResponse({'slope': slope, 'intercept': intercept, 'r': r, 'p': p, 'se': se})
+    xRange = np.amax(xdata) - np.amin(xdata)
+    xStart = np.amin(xdata) - xRange * .1
+    xEnd = np.amax(xdata) + xRange * .1
+    xSteps = int(xRange/0.1)
+    
+    xVals = np.linspace(xStart, xEnd, xSteps)
+    yVals = xVals * slope + intercept
+
+    lineData = []
+    for i in range(len(xVals)):
+        lineData.append({'x':xVals[i], 'y':yVals[i]})
+
+    return JsonResponse({'data':lineData})
