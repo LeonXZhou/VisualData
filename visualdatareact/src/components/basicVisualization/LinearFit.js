529b410ecb6f1cd7638a9e94d3d301023abc963d
LeonXZhou <leon.zhou@queensu.ca>
2022-02-28 16:50:10 -0800

    refactored code to use a custom hook for fitting

--- visualdatareact/src/components/basicVisualization/LinearFit.js
+++ visualdatareact/src/components/basicVisualization/LinearFit.js
@@ -1,103 +1,20 @@
-import { useEffect, useState, } from 'react'
-
 //styles
 import './LinearFit.scss'
 //chart js
-import { Chart } from 'react-chartjs-2'
-import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Tooltip,
-  Legend,
-  CategoryScale,
-  ScatterController,
-} from 'chart.js';
-
-//helpers
-import { calcLinearFit } from '../../helpers/apiHelpers';
-
+import useFit from '../../hooks/useFit';
 
 export default function LinearFit() {
-  ChartJS.register(LinearScale, PointElement, LineElement, CategoryScale, Tooltip, Legend, ScatterController);
-  const [xState, setxState] = useState('1,2.5,3,4,5.6,6');
-  const [yState, setyState] = useState('1,2,3,4.4,5,6');
-  const [lineDataState, setLineDataState] = useState([]);
-  const [slopeInterceptState, setSlopeInterceptState] = useState({ slope: "", intercept: "", slopeErr: "", interceptErr: "" })
-  useEffect(() => {
-    calcLinearFit(xData, yData).then(
-      (res) => {
-        setLineDataState(res.data.data)
-        setSlopeInterceptState(res.data.slopeIntercept)
-      }
-    )
-  }, [xState, yState])
 
-  let xData = xState.split(',')
-  let yData = yState.split(',')
-
-  xData = xData.filter(x => x !== '');
-  yData = yData.filter(y => y !== '');
-
-
-  while (xData.length < yData.length) {
-    xData.push(0)
-  }
-  while (xData.length > yData.length) {
-    yData.push(0)
-  }
-
-  const inputData = [];
-  for (const i in yData) {
-    inputData.push({ x: xData[i], y: yData[i] })
-  }
-
-  const data = {
-    datasets: [
-      {
-        label: 'x value',
-        data: inputData,
-        type: "scatter",
-        backgroundColor: 'rgba(255, 99, 132, 1)',
-        borderColor: 'rgba(255, 99, 132, 1)',
-        pointStyle: 'star',
-      }
-    ],
-  };
-
-
-  if (lineDataState.length > 0) {
-    console.log(lineDataState)
-    data.datasets[1] = {
-      label: 'yvalue dataset',
-      data: lineDataState,
-      type: "line",
-      backgroundColor: '#1e5b94',
-      borderColor: '#1e5b94',
-      pointStyle: 'none',
-      pointRadius: 0,
-      borderWidth: 1,
-    }
-  }
-
-  const options = {
-    plugins: {
-      legend: {
-        display: false,
-      }
-    },
-    events: []
-  }
-
+  const {Chartjsx, xState, setxState, yState, setyState, slopeInterceptState} = useFit();
   return (
     <div className='linearFitContainer'>
 
       <div className='chartContainer'>
-        <Chart
+        {Chartjsx}
+        {/* <Chart
           data={data}
           options={options}
-        />
+        /> */}
       </div>
       <div className="dataAnalysisSectionContainer">
 
